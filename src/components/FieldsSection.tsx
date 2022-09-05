429224d7d0b6e6b19efad8cd5eac8dad5a915d4a
Jonathan Vegas <jonathanvegas@gmail.com>
2022-09-04 21:22:59 -0400

    Date ranges and insert to DB working

--- src/components/FieldsSection.tsx
+++ src/components/FieldsSection.tsx
@@ -1,22 +1,7 @@
 import React, {useState, useEffect} from "react";
 import { Form, Input, Button, DatePicker, Select } from 'antd';
+import moment from "moment";
 import "antd/dist/antd.css"
-
-const { Option } = Select;
-
-// Input
-const onChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-  console.log(e);
-};
-
-// Select
-const onChangeSelect = (value: string) => {
-  console.log(`selected ${value}`);
-};
-// Select
-const onSearch = (value: string) => {
-  console.log('search:', value);
-};
 
 type SizeType = Parameters<typeof Form>[0]['size'];
 
@@ -27,6 +12,94 @@ function FieldsSection() {
     setComponentSize(size);
   };
 
+  const [user_id, setUser_Id] = useState('jonveg');////bring from user profile
+  const [zipOrigin, setZipOrigin] = useState('');
+  const [cityOrigin, setCityOrigin] = useState('');
+  const [zipDestination, setZipDestination] = useState('');
+  const [cityDestination, setCityDestination] = useState('');
+  const [tripMiles, setTripMiles] = useState(0);
+  const [status, setStatus] = useState('');
+  const [date, setDate] = useState(moment().format('YYYY-MM-DD').toString());
+  const [rate, setRate] = useState(0.6); //bring rate value from user profile
+
+  const [form, setForm] = useState({});
+
+  const onChangeSelect = (value: string) => {
+    setStatus(value);
+    };
+
+  const onSearch = (value: string) => {
+    setStatus(value);
+  };
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    switch (e.target.id) {
+      case 'zipOrigin':
+        setZipOrigin(e.target.value);
+      break;
+      case 'cityOrigin':
+        setCityOrigin(e.target.value);
+      break;
+      case 'zipDestination':
+        setZipDestination(e.target.value);
+      break;
+      case 'cityDestination':
+        setCityDestination(e.target.value);
+      break;
+      case 'tripMiles':
+        setTripMiles(Number(e.target.value));
+        console.log(e.target.value);
+      break;
+    
+      default:
+        break;
+    }
+  };
+
+  const onChangeDate = (dateObject: moment.Moment | null, dateString: string) => {
+      setDate(dateString);
+  }
+
+  const onClickButton = () => {
+    setForm( 
+    {
+      user_id: 'jonveg', //get user from profile
+      zipOrigin: zipOrigin,
+      cityOrigin: cityOrigin,
+      zipDestination: zipDestination,
+      cityDestination: cityDestination,
+      tripMiles: tripMiles,
+      status: status,
+      date: date,
+      rate: rate //get rate from profile
+    })
+    console.log(form);
+    formSubmit();
+  } 
+
+  async function formSubmit(){
+    try {
+      //console.log('forSubmit')
+      const results = await fetch("http://localhost:5001/trips/", {
+        method: "POST",
+        headers: {
+          "Content-Type":"application/json"
+        },
+        body: JSON.stringify(form)
+      });
+      console.log(results);
+      const data = await results.json();
+      console.log(data);
+
+    } catch(error){
+      console.log(error);
+    }
+  }
+  
+  useEffect(() => {
+    formSubmit();
+  }, [form])
+
   return(
     <Form id="tripsData-form"
       labelCol={{ span: 4 }}
@@ -36,47 +109,46 @@ function FieldsSection() {
       onValuesChange={onFormLayoutChange}
       size={componentSize as SizeType} >
       <Form.Item label="From Zip Code">
-          <Input placeholder="Zip Code" allowClear onChange={onChange} />
+          <Input id="zipOrigin" placeholder="Zip Code" allowClear onChange={onChange} />
       </Form.Item>
       
       <Form.Item label="Origin City"> 
-        <Input placeholder="Zip Code Origin" allowClear onChange={onChange} />
-      </Form.Item>
+        <Input id="cityOrigin" placeholder="Zip Code Origin" allowClear onChange={onChange} />
+      </Form.Item> 
 
       <Form.Item label="To Zip Code">
-          <Input placeholder="Zip Code" allowClear onChange={onChange} />
+          <Input id="zipDestination" placeholder="Zip Code" allowClear onChange={onChange} />
       </Form.Item>
       
       <Form.Item label="Destination City"> 
-        <Input placeholder="Zipc Code Destination" allowClear onChange={onChange} />
+        <Input id="cityDestination" placeholder="Zipc Code Destination" allowClear onChange={onChange} />
       </Form.Item>
 
       <Form.Item label="Miles"> 
-        <Input placeholder="Miles" allowClear onChange={onChange}/>
+        <Input id="tripMiles" placeholder="Miles" allowClear onChange={onChange}/>
       </Form.Item>
 
       <Form.Item label="DatePicker">
-        <DatePicker />
+        <DatePicker id="date" defaultValue={moment()} onChange = {onChangeDate}  />
       </Form.Item>
 
       <Form.Item label="Status">
         <Select
+          id="status"
           showSearch
           placeholder="Select a status"
           optionFilterProp="children"
           onChange={onChangeSelect}
           onSearch={onSearch}
-          filterOption={(input, option) =>
-            (option!.children as unknown as string).toLowerCase().includes(input.toLowerCase())
-          }
+          // filterOption={(input, option) =>
+          //   (option!.children as unknown as string).toLowerCase().includes(input.toLowerCase())
+          // }
         >
-          <Select.Option value="loaded">Loaded</Select.Option>
-          <Select.Option value="empty">Empty</Select.Option>
+          <Select.Option value="Loaded">Loaded</Select.Option>
+          <Select.Option value="Empty">Empty</Select.Option>
         </Select>
       </Form.Item>
-
-      <Button type="primary" htmlType='submit'>Submit</Button>
-
+      <Button type="primary" htmlType='submit' onClick={onClickButton}>Submit</Button>
     </Form>
   )
 }
--- src/components/TableSection.tsx
+++ src/components/TableSection.tsx
@@ -3,6 +3,8 @@ import type { DatePickerProps } from 'antd';
 import { Table, Space, DatePicker } from 'antd';
 import moment from 'moment';
 import type { ColumnsType } from 'antd/es/table';
+import type { RangePickerProps } from 'antd/es/date-picker';
+
 
 const { RangePicker } = DatePicker;
 const dateFormat = 'YYYY-MM-DD';
@@ -10,8 +12,7 @@ const weekFormat = 'MM/DD';
 const monthFormat = 'YYYY/MM';//****** */
 const customWeekStartEndFormat: DatePickerProps['format'] = value =>
   `${moment(value).startOf('week').format(weekFormat)} ~ ${moment(value)
-    .endOf('week')
-    .format(weekFormat)}`;
+    .endOf('week').format(weekFormat)}`;
 
 let startDateWeek: string = moment(new Date()).startOf('week').format(dateFormat);
 let endDateWeek: string = moment(new Date()).endOf('week').format(dateFormat);
@@ -62,12 +63,17 @@ const columns: ColumnsType<DataType> = [
     title: 'Date',
     dataIndex: 'date',
     key: 'date',
+  
   },
 ];
 
+// const onOk = (value: DatePickerProps['value'] | RangePickerProps['value']) => {
+//   console.log('onOk: ', value);
+// };
+
 function TableSection () {
   
-  const user_id:string = 'jonveg';
+  const user_id:string = 'jonveg'; //bring from user profile
   const [dataTable, setDataTable] = useState([]);
   const fetchData = () =>
     fetch(`http://localhost:5001/trips/${user_id}/${startDateWeek}/${endDateWeek}`)
@@ -77,16 +83,36 @@ function TableSection () {
     .then(data => {
       setDataTable(data);
     })
-
+  
     useEffect(() => {
       fetchData();
     }, [])
-    
+  
+    const onChange: RangePickerProps['onChange'] = (dates, dateStrings) => {
+      if (dates) {
+        startDateWeek = dateStrings[0];
+        endDateWeek = dateStrings[1];
+        fetchData();
+      } else {
+        console.log('Clear');
+      }
+    };
+
+    const onChangeWeek = (value: DatePickerProps['value'] | RangePickerProps['value'], 
+                          dateString: [string, string] | string,) => {
+       
+      let DateWeek: any = (value?.toString());
+      startDateWeek = moment(new Date(DateWeek)).startOf('week').format(dateFormat);
+      endDateWeek = moment(new Date(DateWeek)).endOf('week').format(dateFormat);
+      fetchData();
+      //console.log(moment(new Date(DateWeek)).startOf('week').format(dateFormat))
+    };
+
   return(
     <>
     <Space direction="vertical" size={12}>
-      <DatePicker defaultValue={moment()} format={customWeekStartEndFormat} picker="week" />
-      <RangePicker />
+      <DatePicker defaultValue={moment()} format={customWeekStartEndFormat} picker="week" onChange={onChangeWeek}  />
+      <RangePicker onChange={onChange} />
     </Space>
     <Table columns={columns} dataSource={dataTable} />
     </>
@@ -94,11 +120,3 @@ function TableSection () {
 }
 
 export default TableSection;
-
-// useEffect(() => {
-  //   fetch('http://localhost:5001/trips')
-  //   .then(results => results.json())
-  //   .then(data => setDataTable(data))
-  //   .catch(console.error)
-  // }, [setDataTable]);
-  // console.log(dataTable)
