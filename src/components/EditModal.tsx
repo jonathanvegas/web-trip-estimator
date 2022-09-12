cfcdb70660af088f23f97bc50861724a8526e27d
Jonathan Vegas <jonathanvegas@gmail.com>
2022-09-12 17:31:56 -0400

    fixed css on modal windows

--- src/components/EditModal.tsx
+++ src/components/EditModal.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Input, Modal, DatePicker, Select, Space } from "antd";
+import { Input, Modal, DatePicker, Select, Row, Col } from "antd";
 import moment from 'moment';
 
 interface DataType {
@@ -24,111 +24,162 @@ interface EditModalProp {
 }
 
 const EditModal= ({ visible, setVisible, edit, setEdit, resetEditing, updateTrips}: EditModalProp) => {
-  
+  function handleDate(strDate:string){
+    console.log(strDate)
+    return strDate?.split('T')[0];
+  }
   return (
     <>
       <Modal
         title="Edit Trip Details"
         visible={visible}
         okText="Save"
-        width={625}
+        width={500}
         onCancel={() => resetEditing()}
         onOk={() => {
           updateTrips(edit);
           resetEditing();
         }}
       >
-        <Space style={{display:"flex",gap:10}} >
-        <h5>Zip Code (FROM)</h5>
-        <Input
-          value={edit?.zipOrigin}
-          onChange={(e) => {
-            setEdit((pre: DataType) => {
-              return { ...pre, zipOrigin: e.target.value };
-            });
-          }}
-        />
-        </Space>
-        <Space style={{display:"flex",gap:37}}>
-        <h5>City Origin</h5>
-        <Input
-          value={edit?.cityOrigin}
-          onChange={(e) => {
-            setEdit((pre: DataType) => {
-              return { ...pre, cityOrigin: e.target.value };
-            });
-          }}
-        />
-        </Space>
-        <Space style={{display:"flex",gap:26}}>
-        <h5>Zip Code (TO)</h5>
-        <Input
-          value={edit?.zipDestination}
-          onChange={(e) => {
-            setEdit((pre: DataType) => {
-              return { ...pre, zipDestination: e.target.value };
-            });
-          }}
-        />
-        </Space>
-        <Space style={{display:"flex",gap:10}}>
-        <h5>City Destination</h5>
-        <Input
-          value={edit?.cityDestination}
-          onChange={(e) => {
-            setEdit((pre: DataType) => {
-              return { ...pre, cityDestination: e.target.value };
-            });
-          }}
-        />
-        </Space>
-        <Space style={{display:"flex",gap:75}}>
-        <h5>Miles</h5>
-        <Input 
-          value={edit?.tripMiles}
-          onChange={(e) => {
-            setEdit((pre: DataType) => {
-              return { ...pre, tripMiles: e.target.value };
-            });
-          }}
-        />
-        </Space>
-        <Space style={{display:"flex",gap:73}}>
-        <h5>Rate</h5>
-        <Input
-          value={edit?.rate}
-          onChange={(e) => {
-            setEdit((pre: DataType) => {
-              return { ...pre, rate: e.target.value };
-            });
-          }}
-        />
-        </Space>
-        {/* <DatePicker
-        /> */}
-        <Space style={{display:"flex",gap:76}}>
-        <h5>Date</h5>
-        <Input
-          // value={handleDate(edit?.date)}
-          value={edit?.date}
-          onChange={(e) => {
-            setEdit((pre: DataType) => {
-              return { ...pre, date: e.target.value };
-            });
-          }}
-        />
-        </Space>
-        <Space style={{display:"flex",gap:63}}>
-        <h5>Status</h5>
-        <Input 
-          value={edit?.status}
-          onChange={(e) => {
-            setEdit((pre: DataType) => {
-              return { ...pre, status: e.target.value };
-            });
-          }}
-        />
-        </Space>
+        <Input.Group>
+          <Row gutter={8}>
+            <Col span={5}>
+              <h5>Zip Code (FROM)</h5>
+            </Col>
+            <Col span={8}>
+              <Input
+                value={edit?.zipOrigin}
+                onChange={(e) => {
+                  setEdit((pre: DataType) => {
+                    return { ...pre, zipOrigin: e.target.value };
+                  });
+                }}
+              />
+            </Col>
+          </Row>
+        </Input.Group>
+        <Input.Group>
+          <Row gutter={8}>
+            <Col span={5}>
+              <h5>City Origin</h5>
+            </Col>
+            <Col span={8}>
+              <Input
+              value={edit?.cityOrigin}
+              onChange={(e) => {
+                setEdit((pre: DataType) => {
+                  return { ...pre, cityOrigin: e.target.value };
+                });
+              }}
+            />
+            </Col>
+          </Row>
+        </Input.Group>
+        <Input.Group>
+          <Row gutter={8}>
+            <Col span={5}>
+              <h5>Zip Code (TO)</h5>
+            </Col>
+            <Col span={8}>
+              <Input
+                value={edit?.zipDestination}
+                onChange={(e) => {
+                  setEdit((pre: DataType) => {
+                    return { ...pre, zipDestination: e.target.value };
+                  });
+                }}
+              />
+            </Col>
+          </Row>
+        </Input.Group>
+        <Input.Group >
+          <Row gutter={8}>
+            <Col span={5}>
+              <h5>City Destination</h5>
+            </Col>
+            <Col span={8}>
+              <Input
+                value={edit?.cityDestination}
+                onChange={(e) => {
+                  setEdit((pre: DataType) => {
+                    return { ...pre, cityDestination: e.target.value };
+                  });
+                }}
+              />
+            </Col>
+          </Row>
+        </Input.Group>
+        <Input.Group >
+          <Row gutter={8}>
+            <Col span={5}>
+              <h5>Miles</h5>
+            </Col>
+            <Col span={8}>
+              <Input 
+                value={edit?.tripMiles}
+                onChange={(e) => {
+                  setEdit((pre: DataType) => {
+                    return { ...pre, tripMiles: e.target.value };
+                  });
+                }}
+              />
+            </Col>
+          </Row>
+        </Input.Group>
+
+        <Input.Group>
+          <Row gutter={8}>
+            <Col span={5}>
+              <h5>Rate</h5>
+            </Col>
+            <Col span={8}>
+              <Input
+                value={edit?.rate}
+                onChange={(e) => {
+                  setEdit((pre: DataType) => {
+                    return { ...pre, rate: e.target.value };
+                  });
+                }}
+              />
+            </Col>
+          </Row>
+        </Input.Group>
+        
+        <Input.Group>
+          <Row gutter={8}>
+            <Col span={5}>
+              <h5>Date</h5>
+            </Col>
+            <Col span={8}>
+              <Input
+                value={handleDate(edit?.date)}
+                //value={edit?.date}
+                onChange={(e) => {
+                  setEdit((pre: DataType) => {
+                    return { ...pre, date: e.target.value };
+                  });
+                }}
+              />
+            </Col>
+          </Row>
+        </Input.Group>
+        <Input.Group>
+          <Row gutter={8}>
+            <Col span={5}>
+              <h5>Status</h5>
+            </Col>
+            <Col span={8}>
+              <Input 
+                value={edit?.status}
+                onChange={(e) => {
+                  setEdit((pre: DataType) => {
+                    return { ...pre, status: e.target.value };
+                  });
+                }}
+              />
+            </Col>
+          </Row>
+        </Input.Group>
       </Modal>
     </>
   );
